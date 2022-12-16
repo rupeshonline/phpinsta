c687c1a3bcae20c9bf64a699b7a2cc0f4a37d6cf
rupeshonline <79238629+rupeshonline@users.noreply.github.com>
2022-12-16 14:33:45 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function PMT($interest,$period,$loan_amount){
+  $interest = (float)$interest;
+  $period = (float)$period;
+  $loan_amount = (float)$loan_amount;
+  $period = $period * 12;
+  $interest = $interest / 1200;
+  $amount = $interest * -$loan_amount * pow((1+$interest),$period) / (1 - pow((1+$interest), $period));
+  return $amount;
+}
+
+function showValue($value){
+  echo number_format($value,2);
+}
+
+function showDate($date){
+  echo date('jS F, Y',strtotime($date));
+}
